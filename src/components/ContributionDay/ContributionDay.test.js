091df398d9c1f254c0637193d5e355ebb3d80b96
Guilherme Tassinari <guitassinari@gmail.com>
2020-09-19 14:33:23 -0300

    passes all contribution day tests

--- src/components/ContributionDay/ContributionDay.test.js
+++ src/components/ContributionDay/ContributionDay.test.js
@@ -12,79 +12,79 @@ describe('Contribution day', () => {
     expect(tree).toMatchSnapshot();
   });
 
-  describe('When receives contributionCount prop', () => {
+  describe('When receives count prop', () => {
     describe('and it is 0 (no contribution)', () => {
       test('it matches snapshot', () => {
-        const component = renderer.create(<ContributionDay contributionCount={0} />);
+        const component = renderer.create(<ContributionDay count={0} />);
         const tree = component.toJSON()
       
         expect(tree).toMatchSnapshot();
       })
 
       test('it renders Square with correct background color', () => {
-        const wrapper = shallow(<ContributionDay contributionCount={0} />);
+        const wrapper = shallow(<ContributionDay count={0} />);
         
-        expect(wrapper.find(Square).props().backgroundColor).to.eq("#EBEDF0");
+        expect(wrapper.find(Square).props().backgroundColor).toEqual("#EBEDF0");
       })
     })
 
     describe('and it is between 1 and 2 (low contribution)', () => {
       test('it matches snapshot', () => {
-        const component = renderer.create(<ContributionDay contributionCount={1} />);
+        const component = renderer.create(<ContributionDay count={1} />);
         const tree = component.toJSON()
       
         expect(tree).toMatchSnapshot();
       })
 
       test('it renders Square with correct background color', () => {
-        const wrapper = shallow(<ContributionDay contributionCount={0} />);
+        const wrapper = shallow(<ContributionDay count={1} />);
         
-        expect(wrapper.find(Square).props().backgroundColor).to.eq("#9BE9A8");
+        expect(wrapper.find(Square).props().backgroundColor).toEqual("#9BE9A8");
       })
     })
 
     describe('and it is between 3 and 5 (medium contribution)', () => {
       test('it matches snapshot', () => {
-        const component = renderer.create(<ContributionDay contributionCount={0} />);
+        const component = renderer.create(<ContributionDay count={3} />);
         const tree = component.toJSON()
       
         expect(tree).toMatchSnapshot();
       })
 
       test('it renders Square with correct background color', () => {
-        const wrapper = shallow(<ContributionDay contributionCount={0} />);
+        const wrapper = shallow(<ContributionDay count={3} />);
         
-        expect(wrapper.find(Square).props().backgroundColor).to.eq("#40C463");
+        expect(wrapper.find(Square).props().backgroundColor).toEqual("#40C463");
       })
     })
 
     describe('and it is between 6 and 8 (high contribution)', () => {
       test('it matches snapshot', () => {
-        const component = renderer.create(<ContributionDay contributionCount={0} />);
+        const component = renderer.create(<ContributionDay count={6} />);
         const tree = component.toJSON()
       
         expect(tree).toMatchSnapshot();
       })
 
       test('it renders Square with correct background color', () => {
-        const wrapper = shallow(<ContributionDay contributionCount={0} />);
+        const wrapper = shallow(<ContributionDay count={6} />);
         
-        expect(wrapper.find(Square).props().backgroundColor).to.eq("#30A14E");
+        expect(wrapper.find(Square).props().backgroundColor).toEqual("#30A14E");
       })
     })
 
     describe('and it is higher than 8 (lots of contributions)', () => {
       test('it matches snapshot', () => {
-        const component = renderer.create(<ContributionDay contributionCount={0} />);
+        const component = renderer.create(<ContributionDay count={9} />);
         const tree = component.toJSON()
       
         expect(tree).toMatchSnapshot();
       })
 
       test('it renders Square with correct background color', () => {
-        const wrapper = shallow(<ContributionDay contributionCount={0} />);
+        const wrapper = shallow(<ContributionDay count={9} />);
         
-        expect(wrapper.find(Square).props().backgroundColor).to.eq("#9BE9A8");
+        expect(wrapper.find(Square).props().backgroundColor).toEqual("#216E39");
       })
     })
   })
